7d116e15da2d58e241ae9f965f85a15ee117cec3
Irvin Cortez <irvincotez67@gmail.com>
2025-09-04 00:03:44 -0600

    fix: 🐛 namespace
    Signed-off-by: Irvin Cortez <irvincotez67@gmail.com>

--- src/Core/ClientGuzzleHttp.php
+++ src/Core/ClientGuzzleHttp.php
@@ -1,5 +1,6 @@
 <?php
-namespace Src\Core;
+
+namespace Momotolabs\SdkBiller\Core;
 
 use GuzzleHttp\Client as Guzzle;
 
--- src/Core/Config.php
+++ src/Core/Config.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Src\Core;
+namespace Momotolabs\SdkBiller\Core;
 
 class Config
 {
